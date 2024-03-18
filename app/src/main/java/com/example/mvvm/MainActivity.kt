69328ca477b1b4b91234839ed97b95640a867a03
DesenvolvedorAdr <adrianobabaganush@hotmail.com>
2024-03-17 23:18:21 -0300

    MVVM

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -2,17 +2,28 @@ package com.example.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.View
+import android.widget.Toast
 import com.example.mvvm.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity(),View.OnClickListener {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnLogin.setOnClickListener(this)
 
     }
+    override fun onClick(v: View) {
+        if (v.id == R.id.btnLogin){
+            Toast.makeText(this, "Button funcionando!!!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
